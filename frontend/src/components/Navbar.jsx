08e90f312482b420fc07844fcb2992c43a063ae3
sumitkrravi <ravisumit1425@gmail.com>
2024-12-25 19:40:38 +0530

    update by sumit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,45 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup on unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const navItems = document.querySelectorAll('nav ul li a');
+
+    navItems.forEach((item) => {
+      item.addEventListener('mouseover', () => {
+        item.classList.add('hovered');
+      });
+
+      item.addEventListener('mouseout', () => {
+        item.classList.remove('hovered');
+      });
+    });
+
+    return () => {
+      navItems.forEach((item) => {
+        item.removeEventListener('mouseover', () => {});
+        item.removeEventListener('mouseout', () => {});
+      });
+    };
+  }, []);
+
   return (
     <>
-      <nav className=''>
+      <nav className={isScrolled ? 'scrolled' : ''}>
         <div>
-          <Link to="/"><h2 className="">BlogOasis</h2></Link>
+          <Link to="/"><h2>BlogOasis</h2></Link>
         </div>
         <div>
-          <ul className=''>
-            <li className=''>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li className=''>
-              <a href="#authors">Authors</a>
-            </li>
-            <li className=''>
-              <a href="#about">About</a>
-            </li>
-            <li className=''>
-              <Link to="/contribute">Contribute</Link>
-            </li>
-            <li className=''>
-              <a href="#">Help</a>
-            </li>
+          <ul>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><a href="#authors">Authors</a></li>
+            <li><a href="#about">About</a></li>
+            <li><Link to="/contribute">Contribute</Link></li>
+            <li><a href="#">Help</a></li>
           </ul>
         </div>
-
-        <div className="">
-          <Link to="/createblog" className="">
-            Create Blog
-          </Link>
-          <Link to="/signup" className="">
-            Signup
-          </Link>
+        <div>
+          <Link to="/createblog" className="btn">Create Blog</Link>
+          <Link to="/signup" className="btn">Signup</Link>
         </div>
       </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
