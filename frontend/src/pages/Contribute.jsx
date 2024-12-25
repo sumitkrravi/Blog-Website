0fb244f3d85a096e18651bc8a5fd8d53582dcc75
sumitkrravi <ravisumit1425@gmail.com>
2024-12-25 19:49:26 +0530

    update by sumit

--- frontend/src/pages/Contribute.jsx
+++ frontend/src/pages/Contribute.jsx
@@ -1,34 +1,51 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import './Contribute.css'
 
 function Contribute() {
   return (
     <>
-    <Navbar />
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto totam
-        vitae excepturi praesentium? Saepe ipsam consectetur magni rem ea
-        doloribus ut, harum perspiciatis nesciunt, fuga expedita possimus fugiat
-        corporis quidem excepturi odit adipisci esse sed! Id ducimus quisquam
-        perspiciatis soluta neque fuga voluptas? Eligendi facere corporis magnam
-        accusamus veritatis consequatur, totam assumenda, quidem odio dolore
-        esse. Atque repellendus quos eos necessitatibus excepturi. Modi impedit
-        doloribus obcaecati, nulla inventore aliquam quia laudantium odio, totam
-        fugiat, est perferendis temporibus error alias. Provident quae earum
-        animi porro beatae officia dolores? Fugit nostrum quo maxime pariatur
-        consequatur doloribus tempora a consectetur, velit ipsam maiores
-        consequuntur numquam iure molestiae dolores ullam blanditiis id ab rem
-        quasi. Reprehenderit, vel sed voluptate maiores similique totam cumque
-        veniam modi ratione. Quisquam fugiat excepturi, adipisci similique
-        dignissimos nostrum aut temporibus. Quae necessitatibus aspernatur at
-        facilis voluptatibus provident repudiandae repellat fugit, dolorem alias
-        quia, asperiores dolorum eius vero minus nihil odio, nam sint eveniet
-        veritatis. Minus placeat qui ipsam maiores, voluptatibus repudiandae
-        modi expedita debitis at fugit officia adipisci nisi quis rerum ab! Cum,
-        ad. Quisquam cumque quo nisi doloribus quia, quam, provident neque nihil
-        eaque est deserunt nulla voluptate maiores. Temporibus sapiente
-        provident architecto, fugiat possimus repellat quisquam nihil?
-      </p>
+      <Navbar />
+
+      <div className="category-filters">
+        <button onClick={() => filterContributors('design')}>Designers</button>
+        <button onClick={() => filterContributors('content')}>Content Creators</button>
+        <button onClick={() => filterContributors('dev')}>Developers</button>
+      </div>
+
+
+      <div className="contribution-page">
+
+        <section className="contribution-grid">
+          <div className="contributor-card">
+            <img src="path_to_image" alt="Contributor Name" className="profile-img" />
+            <h3>Contributor Name</h3>
+            <p>Role: Blog Writer</p>
+            <a href="https://github.com/contributor" target="_blank">View Profile</a>
+            <div className="testimonial">
+              <p>"Contributing to this project was an amazing experience. It helped me improve my skills and meet great people!"</p>
+              <h4>- Contributor Name</h4>
+            </div>
+          </div>
+        </section>
+
+
+
+        <div className="contribution-grid">
+          <section className="contributor-card">
+            <img src="path_to_image" alt="Contributor Name" className="profile-img" />
+            <h3>Contributor Name</h3>
+            <p>Role: Blog Writer</p>
+            <a href="https://github.com/contributor" target="_blank">View Profile</a>
+
+            <div className="testimonial">
+              <p>"Contributing to this project was an amazing experience. It helped me improve my skills and meet great people!"</p>
+              <h4>- Contributor Name</h4>
+            </div>
+          </section>
+        </div>
+      </div>
+
 
       <Footer />
     </>
